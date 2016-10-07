6dedc294ba43c70063ea9eccc2a021c9f1d91283
Aztrand <oscar_aspestrand@hotmail.com>
2016-10-07 19:54:01 +0200

    tt

--- Lab1.m
+++ Lab1.m
@@ -22,7 +22,7 @@ y = randn([1 N]);
 
 figure()
 subplot(1,2,1)
-plot(x,y,'.') %equiprobability lines: either a circle or diagonals?
+plot(x,y) %equiprobability lines: either a circle or diagonals?
 title('Normal distribution')
 xlabel('X')
 ylabel('Y')
@@ -31,7 +31,7 @@ a=sqrt(3);
 u = a*rand([1 N])
 v = a*rand([1 N])
 subplot(1,2,2)
-plot(u,v,'.')
+plot(u,v)
 title('Uniform distribution')
 xlabel('U')
 ylabel('V')
@@ -149,26 +149,59 @@ u=rand([1 N]);
 z=@(a) x.*a+sqrt(1-a.^2).*y;
 zv = z(0.7);
 
-dz = 0.1;
-zh = 0.5;
-i=0;
-X=[];
-Z=[];
+
+dz = 0.1
+zh = ([0.5 -0.5]);
+i=[1 1];
+X1=[];
+Z1=[];
 for k=1:2000
-    if zv(k)>zh-dz && zv(k)<zh+dz
-        %plot(x(k),y(k),'.')
-        i=i+1;
-        %hold on
-        X(i)=x(k); 
-        Z(i)=zv(k);
+    for l=1:2
+        if zv(k)>zh(l)-dz && zv(k)<zh(l)+dz
+            %plot(x(k),y(k),'.')
+            
+            %hold on
+            X1(l,i)=x(k); 
+            Z1(l,i)=zv(k);
+            i(l)=i(l)+1;
+        end
     end
 end
-
+figure()
 subplot(1,2,1)
-plot(Z,X,'.')
+hist(X1(1,:),100)
+title('X/Z - alpha = 0.7, z=0.5')
 subplot(1,2,2)
-hist(X,100)
-title('U/V')
+hist(X1(2,:),100)
+title('X/Z - alpha = 0.7, z=-0.5')
+
+
+zv = z(-0.7);
+
+dz = 0.1;
+zh = ([0.5 -0.5]);
+i=([1 1]);
+X2=[];
+Z2=[];
+for k=1:2000
+    for l=1:2
+        if zv(k)>zh(l)-dz && zv(k)<zh(l)+dz
+            %plot(x(k),y(k),'.')
+            
+            %hold on
+            X2(l,i)=x(k); 
+            Z2(l,i)=zv(k);
+            i(l)=i(l)+1;
+        end
+    end
+end
+figure
+subplot(1,2,1)
+hist(X2(1,:),100)
+title('X/Z - alpha = -0.7, z=0.5')
+subplot(1,2,2)
+hist(X2(2,:),100)
+title('X/Z - alpha = -0.7, z=-0.5')
 
 %%
 %4.1 Ensemble and Time Averages
@@ -184,6 +217,7 @@ plot(EA)
 hold on
 plot(TA)
 diff = mean(EA)-mean(TA)
+legend('Ensemble Average','Time Average')
 
 %The averages do not hold the same values on each sample, however the main
 %value of both EA and TA hold s the same average value.
@@ -195,13 +229,25 @@ N=256;
 K=N;
 
 X=randn(N,K);
+subplot(2,2,1)
 plot(X(1,:),X(2,:),'.')
-sum=0;
+title('n1=1, n2=2')
+subplot(2,2,2)
+plot(X(10,:),X(20,:),'.')
+title('n1=10 , n2=20')
+subplot(2,2,3)
+plot(X(5,:),X(6,:),'.')
+title('n1=5 , n2=6')
+subplot(2,2,4)
+plot(X(70,:),X(140,:),'.')
+title('n1=70 , n2=140')
+sum=([0 0 0 0]);
 
-for k=1:256
-    r(k)=X(1,k).*X(2,k);
-    sum=sum+r(k);
-end
+    for k=1:256
+        r(k)=X(1,k).*X(2,k);
+        sum=sum+r(k);
+    end
+    
 
 r=1/K*sum;
 
@@ -220,6 +266,8 @@ x=filter(1,[1 -1],W)
 
 figure()
 plot(x)
+xlabel('n')
+ylabel('X(n)')
 figure()
 subplot(2,2,1)
 plot(x(10,:),x(9,:),'.')
@@ -253,16 +301,11 @@ plot(x(200,:),x(190,:),'.')
 W=randn(N,K);
 x=filter(1,[1 -1],W)
 sum=0;
-
 for n=2:256
-    for k=1:256
-        r=x(n,k).*x(n-1,k);
-        sum=sum+r;
-    end
-    result2(n) = mean(x(n)*x(n-1));
-    result(n)=sum;
+    r(n-1)=x(n,:)*x(n-1,:)'/K;
+    result2(n)=n-1;
+    result(n)=r(n-1);
 end
-result = 1/K * result;
 
 figure()
 for n=2:256
@@ -342,41 +385,35 @@ x=filter(1,[1 -0.9],W)
 sum=0;
 
 for n=2:256
-    for k=1:256
-        r=x(n,k).*x(n-1,k);
-        sum=sum+r;
-    end
-    result2(n) = mean(x(n)*x(n-1));
-    result(n)=sum;
+    r(n-1)=x(n,:)*x(n-1,:)'/K;
+    result2(n) = (1-0.81^n)/(1-0.81)
+    result(n)=r(n-1);
 end
-result = 1/K * result;
 
 figure()
 for n=2:256
-    subplot(1,2,1)
-    plot(result(n),result(n-1),'.')
-    title('Practical')
+    plot(result)
     hold on
-    subplot(1,2,2)
-    plot(result2(n),result2(n-1),'.')
-    title('Theoretical')
+    plot(result2)
     hold on
 end
+title('Practical vs Theoretical')
+legend('Practical','Theoretical')
 
 %One realizaion
-sum=0;
-for n=2:256
-    for k=1:1
-        r=x(n,k).*x(n-1,k);
-        sum=sum+r;
-    end
-    %result2(n) = mean(x(n)*x(n-1));
-    result(n)=sum;
-end
-result = 1/K * result;
-
-figure()
-for n=2:256
-    plot(result(n),result(n-1),'.')
-    hold on
-end
+% sum=0;
+% for n=2:256
+%     for k=1:1
+%         r=x(n,k).*x(n-1,k);
+%         sum=sum+r;
+%     end
+%     %result2(n) = mean(x(n)*x(n-1));
+%     result(n)=sum;
+% end
+% result = 1/K * result;
+% 
+% figure()
+% for n=2:256
+%     plot(result(n),result(n-1),'.')
+%     hold on
+% end
